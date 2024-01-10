321cd398cc2af33adf4891c3a88812afd5fb3ebd
swapnilghore <swapnilghore55@gmail.com>
2024-01-10 14:54:41 +0530

    Desing First Page

--- Ui-Development/src/app/app.module.ts
+++ Ui-Development/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatButtonToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
